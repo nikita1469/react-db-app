5936e182c7fc258112478bb2263e30161bbdcbff
nikita1469 <pogrebnoi.nikita2505@gmail.com>
2020-10-26 13:39:32 +0300

    error-indicator

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './spinner.css'
+
+const Spinner = () => {
+   return (
+      <div className="loadingio-spinner-double-ring-qoyfy05qbtk">
+         <div className="ldio-m1638tsocdi">
+            <div></div>
+            <div></div>
+         </div>
+      </div>
+   )
+}
+
+export default Spinner
+
+
